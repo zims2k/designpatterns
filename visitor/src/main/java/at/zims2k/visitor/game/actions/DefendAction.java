9f376e237e6f2f553634ecd403ee03617e3e27ad
Andreas Zimmermann <a_zimmermann@gmx.at>
2024-11-29 00:24:20 +0100

    AZ-xxxx De-simplify, because it is not as simple as I thought

--- visitor/src/main/java/at/zims2k/visitor/game/actions/DefendAction.java
+++ visitor/src/main/java/at/zims2k/visitor/game/actions/DefendAction.java
@@ -1,9 +1,27 @@
 package at.zims2k.visitor.game.actions;
 
+import at.zims2k.visitor.game.units.Archer;
+import at.zims2k.visitor.game.units.Cavalry;
+import at.zims2k.visitor.game.units.Spearman;
+
 public class DefendAction implements UnitAction {
     @Override
     public String toString() {
         return this.getClass().getSimpleName();
     }
 
+    @Override
+    public void visitArcher(Archer archer) {
+
+    }
+
+    @Override
+    public void visitCavalry(Cavalry cavalry) {
+
+    }
+
+    @Override
+    public void visitSpearman(Spearman spearman) {
+
+    }
 }
--- visitor/src/main/java/at/zims2k/visitor/game/actions/UnitAction.java
+++ visitor/src/main/java/at/zims2k/visitor/game/actions/UnitAction.java
@@ -3,22 +3,9 @@ package at.zims2k.visitor.game.actions;
 import at.zims2k.visitor.game.units.Archer;
 import at.zims2k.visitor.game.units.Cavalry;
 import at.zims2k.visitor.game.units.Spearman;
-import at.zims2k.visitor.game.units.Unit;
 
 public interface UnitAction {
-    default void visitArcher(Archer archer) {
-        System.out.printf("Performing action %s to archer %s%n", this, archer);
-    }
-
-    default void visitCavalry(Cavalry cavalry) {
-        System.out.printf("Performing action %s to archer %s%n", this, cavalry);
-    }
-
-    default void visitSpearman(Spearman spearman) {
-        System.out.printf("Performing action %s to archer %s%n", this, spearman);
-    }
-
-    default void visitUnit(Unit unit){
-
-    }
+    void visitArcher(Archer archer);
+    void visitCavalry(Cavalry cavalry);
+    void visitSpearman(Spearman spearman);
 }
