60c53e8e9c5b0cdd0e1eb2e8bbea4e27b0080033
Andreas Zimmermann <a_zimmermann@gmx.at>
2024-12-03 21:24:48 +0100

    AZ-xxxx Cleanup all stuff. The example does not really show the visitor pattern

--- visitor/src/main/java/at/zims2k/visitor/game/units/Unit.java
+++
@@ -1,8 +0,0 @@
-package at.zims2k.visitor.game.units;
-
-import at.zims2k.visitor.game.actions.UnitVisitor;
-
-public interface Unit {
-    /** visitor pattern implementation */
-    void accept(UnitVisitor unitAction);
-}
